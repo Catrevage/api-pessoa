70bd5b7d47e7d196ae7005219a447a438fc4dfdc
Catrevage <damiaovcjr@hotmail.com>
2023-09-27 10:27:38 -0300

    first commit

---
+++ src/test/java/pessoa/com/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package pessoa.com.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
